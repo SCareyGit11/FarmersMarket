b842536ec46d6af7d895bde51bba0dcfab40de70
crayonshin <kikaider@gmail.com>
2016-10-25 14:34:10 -0700

    added javascript file

---
+++ apps/farmers/static/farmers/js/farket.js
@@ -0,0 +1,18 @@
+function findMarkets(zip) {
+    console.log(zip);
+    $.get("http://search.ams.usda.gov/farmersmarkets/v1/data.svc/zipSearch?zip=" + zip, function(response){getMarketData(response);}, "json");
+    function getMarketData(response) {
+        marketArr = [];
+        mapArr = [];
+        for(var i = 0; i < response["results"].length; i++) {
+            marketArr.push(response["results"][i]["id"]);
+        }
+        for(j = 0; j < marketArr.length; j++) {
+            $.get("http://search.ams.usda.gov/farmersmarkets/v1/data.svc/mktDetail?id=" + marketArr[j], function(response){getMapData(response);}, "json");
+        }
+        function getMapData(response) {
+            mapArr.push(response["marketdetails"]["GoogleLink"]);
+        }
+        console.log(mapArr);
+    }
+}
