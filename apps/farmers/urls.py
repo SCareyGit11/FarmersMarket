630946122d6329776ee3e9c61fd96261b62547f2
maheedhar.gunturu <mailmahee@gmail.com>
2016-10-25 14:47:01 -0700

    changes for login registration

--- apps/farmers/urls.py
+++ apps/farmers/urls.py
@@ -1,7 +1,12 @@
 from django.conf.urls import url, include
-from . import views
+from views import index,show_fMarket
+from ..login_reg_app.views import login, register, success, logout
 
 urlpatterns = [
-    url(r'^', views.index),
-    url(r'^', views.results)
+    url(r'^$', index),
+    url(r'^main$', show_fMarket),
+    url(r'^login$', login),
+    url(r'^register$', register),
+    url(r'^success$', success),
+    url(r'^logout$', logout),
 ]
--- apps/farmers/views.py
+++ apps/farmers/views.py
@@ -2,7 +2,13 @@ from django.shortcuts import render, redirect
 
 # Create your views here.
 def index(request):
-    return render(request, 'farmers/index.html')
+    if 'user' in request.session:
+        return redirect('/main')
+    else:
+        return render(request,'login_reg_app/index.html')
 
 def results(request):
     return render(request, 'farmers/results.html')
+
+def show_fMarket(request):
+    return render(request, 'farmers/index.html')
