7431af2571411bd51d6e4d80694c85307807c617
maheedhar.gunturu <mailmahee@gmail.com>
2016-10-25 15:06:27 -0700

    Resolving merge conflict

--- apps/farmers/urls.py
+++ apps/farmers/urls.py
@@ -1,5 +1,4 @@
-<<<<<<< HEAD
-from django.conf.urls import url, include
+from django.conf.urls import url
 from views import index,show_fMarket
 from ..login_reg_app.views import login, register, success, logout
 
@@ -10,12 +9,5 @@ urlpatterns = [
     url(r'^register$', register),
     url(r'^success$', success),
     url(r'^logout$', logout),
-=======
-from django.conf.urls import url
-from . import views
-
-urlpatterns = [
-    url(r'^$', views.index),
-    url(r'^results$', views.results)
->>>>>>> 146fd5965a83c3e0c64ed5f2be3265633cd7d25d
+    rl(r'^results$', results)
 ]
